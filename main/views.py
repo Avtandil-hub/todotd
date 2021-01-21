4b37d98969fea5aecb1e1befe93a2fe095aecd22
admin <admin@gmail.com>
2021-01-22 02:24:55 +0600

    home work

--- main/views.py
+++ main/views.py
@@ -9,3 +9,12 @@ def go(reguest):
 
 def third(reguest):
     return HttpResponse('This is page test3')
+
+def add(reguest):
+    return render(reguest, 'add.html')
+
+def tap(reguest):
+    return render(reguest, 'tap.html')
+
+def apk(reguest):
+    return render(reguest, 'apk.html')
--- todo/urls.py
+++ todo/urls.py
@@ -16,15 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage,go
-from main.views import third
+from main.views import third,add,tap,apk
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name= 'home'),
-    path('go', go, name= ' go'),
-    path('test3', third)
+    path('go', go, name= 'go'),
+    path('test3', third),
+    path('add', add),
+    path('tap', tap),
+    path('apk', apk),
+
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
