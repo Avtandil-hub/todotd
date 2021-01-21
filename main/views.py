aad6b9d51d432bae7778fab3c867a9af2705f27b
admin <admin@gmail.com>
2021-01-21 22:26:46 +0600

    set static and media urls and roots

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def homepage(reguest):
-    return HttpResponse('Hello')
+    return render(reguest, 'index.html')
 
 def go(reguest):
     return HttpResponse('This is my first page')
--- todo/urls.py
+++ todo/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from main.views import homepage,go
 from main.views import third
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +26,5 @@ urlpatterns = [
     path('', homepage, name= 'home'),
     path('go', go, name= ' go'),
     path('test3', third)
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
