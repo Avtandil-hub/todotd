f06075ec1ebd1924a005a832f40465641879ec68
admin <admin@gmail.com>
2021-01-23 18:24:13 +0600

    admin

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from .models import TODO
 
 # Create your views here.
 def homepage(reguest):
@@ -18,3 +19,7 @@ def tap(reguest):
 
 def apk(reguest):
     return render(reguest, 'apk.html')
+
+def test(reguest):
+    todo_list = TODO.objects.all()
+    return render(reguest,'test.html')
